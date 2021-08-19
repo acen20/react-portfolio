5e7bc1279261803827bb6b3fa5090510a801750b
acen20 <ahsen1330@gmail.com>
2021-08-19 17:18:19 +0500

    Routine push

--- src/Header.js
+++ src/Header.js
@@ -7,18 +7,24 @@ function Header() {
     <div>
       <header className="header">
         <div className="intro-container">
-          <h3>Ahsen Nazir</h3>
-          <h1>Web & AI Engineer</h1>
-          <p>
-            Ahsen Nazir is an entrepreneur and business magnate. He is the
-            founder, CEO, and Chief Engineer at SpaceX; early stage investor,
-            CEO, and Product Architect of Tesla, Inc.; founder of The Boring
-            Company; and co-founder of Neuralink and OpenAI. A centibillionaire,
-            Ahsen is one of the richest people in the world.
-          </p>
+          <p>Some brilliance will be placed here</p>
+          <div className="intro">
+            <h3>Ahsen Nazir</h3>
+            <h1>Web & AI Engineer</h1>
+            <p>
+              Ahsen Nazir is an entrepreneur and business magnate. He is the
+              founder, CEO, and Chief Engineer at SpaceX; early stage investor,
+              CEO, and Product Architect of Tesla, Inc.; founder of The Boring
+              Company; and co-founder of Neuralink and OpenAI. A
+              centibillionaire, Ahsen is one of the richest people in the world.
+            </p>
+          </div>
+          <div>
+            <h5>Social links will be placed here</h5>
+          </div>
         </div>
         <div className="slider-container">
-          <RingLoader size={100} />
+          <RingLoader size={100} color={"white"} />
         </div>
       </header>
     </div>
